2c75095d8299ccba5fd381d971d4b01070235649
Alexander Beliaev <beliaevs@gmail.com>
2025-07-27 14:06:09 +0300

    added variadic templates parameter pack capture and expansion

--- variadic_idioms/main.cpp
+++ variadic_idioms/main.cpp
@@ -1,6 +1,19 @@
+#include <concepts>
 #include<iostream>
+
+void print_strings(std::convertible_to<std::string_view> auto&& ...s)
+{
+    for (auto v : std::initializer_list<std::string_view>{ s... })
+        std::cout << v << " ";
+    std::cout << std::endl;
+}
 
 int main()
 {
     std::cout << "variadic templates idioms for c++20\n";
+
+    {
+        print_strings("one", std::string{ "two" });
+    }
+    return 0;
 }
